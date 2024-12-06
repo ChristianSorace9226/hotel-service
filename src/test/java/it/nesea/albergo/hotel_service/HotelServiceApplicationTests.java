3706eb3c60ac3f2d87cef30f35ada30222fb982f
Claca06 <clauscal.cc@gmail.com>
2024-12-06 17:23:25 +0100

    FIRST-COMMIT

---
+++ src/test/java/it/nesea/albergo/hotel_service/HotelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.nesea.albergo.hotel_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
